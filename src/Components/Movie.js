1cbffb16513cc27d4a02823f2cc91f3f236a6d1a
ToFJ <jehletom@hotmail.com>
2022-01-11 18:05:36 +0100

    fixed width on movie cart

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -5,7 +5,7 @@ const Movie = ({ Title, Year, Poster, imdbID }) => {
   return (
     <div className="m-4 pb-8 relative group cursor-pointer">
       <img src={Poster} alt={Title} className="h-96 w-80 object-cover " />
-      <div className="absolute text-white bottom-8 p-4 w-80 isolate bg-opacity-70 bg-slate-900 hidden group-hover:block ">
+      <div className="absolute text-white bottom-8 p-4 w-full isolate bg-opacity-70 bg-slate-900 hidden group-hover:block ">
         <h3>{Title}</h3>
         <p>{Year}</p>
         <Link to={`/movie/${imdbID}`}>
